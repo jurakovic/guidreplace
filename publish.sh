1ee2181e77c40cdb361f69cf1c8f614532b15b5e
jurakovic <17744091+jurakovic@users.noreply.github.com>
2025-03-12 22:52:54 +0100

    revert debugging, cleanup

--- publish.sh
+++ publish.sh
@@ -82,10 +82,7 @@ function release() {
     if [ ! $yn = "y" ]; then exit; fi
   fi
 
-  #git checkout release >/dev/null 2>&1 && git pull || git checkout -b release origin/release >/dev/null 2>&1 || git checkout --orphan release && git reset --hard
-  git checkout -b release origin/release --force
-  git branch -r
-  git show-ref refs/remotes/origin/release
+  git checkout release >/dev/null 2>&1 && git pull || git checkout -b release origin/release --force >/dev/null 2>&1 || git checkout --orphan release && git reset --hard
 
   local url="https://github.com/jurakovic/guidreplace/releases/tag/v$VERSION"
 
@@ -97,7 +94,6 @@ function release() {
   echo "URL:       $url"       >> version
 
   git add version
-  git status
   git commit -m "v$VERSION ($BRANCH)"
   git push --set-upstream origin release
 
