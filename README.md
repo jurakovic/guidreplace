0f93510bfde773dc69aa448583cedb7721f862a0
jurakovic <17744091+jurakovic@users.noreply.github.com>
2025-03-14 22:51:00 +0100

    Resetup repo, upgrade to .net 9, add cli (#1)

---
+++ README.md
@@ -0,0 +1,138 @@
+
+# Guid Replace
+
+`guidrep` is a simple command-line tool for replacing GUIDs (UUIDs) in text files or standard input. It reads the input, replaces all GUIDs with new randomly generated ones, and ensures that any repeated GUIDs in the original are replaced with the same new GUID.
+
+This tool is especially useful for preparing JSON data for re-import, anonymizing data exports, or resetting identifiers in configuration files.
+
+### Features
+
+- **File Processing**
+	- replace GUIDs in a specified file, with options for in-place editing or outputting to a new file
+- **Standard Input/Output Support**
+	- works with piped input and outputs to standard output
+- **Flexible Output**
+	- save the results to a specified output file, or automatically generate a new filename based on the original
+- **Cross-Platform**
+	- runs on Windows, Linux, and macOS
+
+### Download
+
+[![GitHub Release](https://img.shields.io/github/v/release/jurakovic/guidreplace)](https://github.com/jurakovic/guidreplace/releases/latest)
+
+### Usage
+
+```text
+$ guidrep -h
+Description:
+  Guid Replace tool
+
+Usage:
+  guidrep [<inputFile>] [options]
+
+Arguments:
+  <inputFile>  The input file to process. If not specified, reads from standard input.
+
+Options:
+  -i, --in-place         Edit the input file in place
+  -o, --output <output>  The output file to write the result to
+  -q, --quiet            Do not output messages to standard output
+  --version              Show version information
+  -?, -h, --help         Show help and usage information
+```
+
+Replace GUIDs in a file and save to a new file
+
+```
+guidrep input.txt -o output.txt
+```
+
+Replace GUIDs in a file and save to a new automatically created file
+
+```
+guidrep input.txt
+```
+
+Edit a file in place
+
+```
+guidrep -i input.txt
+```
+
+Using standard input and output
+
+```
+cat input.txt | guidrep -q > output.txt
+```
+
+Using standard input and save to a new file
+
+```
+cat input.txt | guidrep -o output.txt
+```
+
+### Example
+
+```
+cat <<EOF | ./guidrep.exe -q
+{
+  "id": "011df4e1-1962-44f0-a21a-06674f4c2374",
+  "data": [
+    {
+      "id": "d49987ee-595c-4fb3-8cea-8da7f281fdbf",
+      "nodeId": "011df4e1-1962-44f0-a21a-06674f4c2374",
+      "state": [
+        {
+          "id": "9f5ad759-f9b5-4f2a-a8ec-8979fe05656f",
+          "typeId": "d49987ee-595c-4fb3-8cea-8da7f281fdbf"
+        }
+      ]
+    }
+  ]
+}
+EOF
+```
+
+Output:
+
+```
+{
+  "id": "76393731-f856-4bc9-9dca-d7aea4f4fb2a",
+  "data": [
+    {
+      "id": "e88465f8-98f1-417b-894c-ac4549083ffe",
+      "nodeId": "76393731-f856-4bc9-9dca-d7aea4f4fb2a",
+      "state": [
+        {
+          "id": "256422d0-a34e-4a8f-8411-e7a5edc6d444",
+          "typeId": "e88465f8-98f1-417b-894c-ac4549083ffe"
+        }
+      ]
+    }
+  ]
+}
+```
+
+### Build from source
+
+1. Make sure you have installed [.NET SDK](https://dotnet.microsoft.com/en-us/download) and other [prerequisites](https://learn.microsoft.com/en-us/dotnet/core/deploying/native-aot/?tabs=linux-ubuntu%2Cnet8#prerequisites) for Native AOT deployment
+
+2. Clone the repository
+
+	```bash
+	git clone https://github.com/jurakovic/guidreplace.git
+	cd guidreplace
+	```
+
+3. Build (publish) with `dotnet`
+
+	```bash
+	cd src
+	dotnet publish -c Release -r linux-x64 --self-contained
+	bin/Release/net9.0/linux-x64/publish/guidrep -h
+	# mv guidrep to desired path
+	```
+
+### Credits
+
+Inspired by [this](https://stackoverflow.com/questions/2201740/replacing-all-guids-in-a-file-with-new-guids-from-the-command-line) Stack Overflow question and answers.
---
+++ src/CommandBuilder.cs
@@ -0,0 +1,163 @@
+using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GuidReplace
+{
+	public static class CommandBuilder
+	{
+		public static RootCommand CreateRootCommand()
+		{
+			var rootCommand = new RootCommand("Guid Replace tool\nFor more information, visit https://github.com/jurakovic/guidreplace");
+
+			var inPlaceOption = new Option<bool>(["--in-place", "-i"], "Edit the input file in place");
+
+			var inputFileArgument = new Argument<string>("inputFile", "The input file to process. If not specified, reads from standard input.")
+			{
+				Arity = ArgumentArity.ZeroOrOne
+			};
+
+			var outputFileOption = new Option<string>(["--output", "-o"], "The output file to write the result to");
+
+			var quietOption = new Option<bool>(["--quiet", "-q"], "Do not output messages to standard output");
+
+			rootCommand.Add(inPlaceOption);
+			rootCommand.Add(inputFileArgument);
+			rootCommand.Add(outputFileOption);
+			rootCommand.Add(quietOption);
+
+			rootCommand.AddValidator(result =>
+			{
+				if (result.GetValueForOption(outputFileOption) != null && result.GetValueForOption(inPlaceOption))
+					result.ErrorMessage = "Options --output and --in-place cannot be used together.";
+			});
+
+			rootCommand.SetHandler(ExecuteAsync, inputFileArgument, inPlaceOption, outputFileOption, quietOption);
+
+			return rootCommand;
+		}
+
+		private static async Task<int> ExecuteAsync(string inputFilename, bool inPlaceReplace, string outputFilename, bool quiet)
+		{
+			//System.Diagnostics.Debugger.Launch();
+
+			string inputText;
+
+			if (!String.IsNullOrEmpty(inputFilename))
+			{
+				if (!File.Exists(inputFilename))
+				{
+					if (!quiet)
+						Console.Error.WriteLine($"File not found: {inputFilename}");
+					return 1;
+				}
+
+				inputText = await File.ReadAllTextAsync(inputFilename);
+			}
+			else
+			{
+				if (Console.IsInputRedirected)
+				{
+					inputText = await Console.In.ReadToEndAsync();
+				}
+				else
+				{
+					if (!quiet)
+						Console.Error.WriteLine("Error: No input file specified and no data in stdin. Run 'guidrep -h' for usage.");
+					return 1;
+				}
+			}
+
+			if (String.IsNullOrWhiteSpace(inputText))
+			{
+				if (!quiet)
+					Console.Error.WriteLine($"Input text empty");
+				return 1;
+			}
+
+			string outputText = ReplaceGuids(inputText, out int matchesCount, out int pairsCount);
+
+			if (matchesCount <= 0)
+			{
+				if (!quiet)
+					Console.Error.WriteLine($"No guids in file");
+				return 1;
+			}
+
+			if (!string.IsNullOrEmpty(outputFilename))
+			{
+				await File.WriteAllTextAsync(outputFilename, outputText);
+			}
+			else if (inPlaceReplace && !String.IsNullOrEmpty(inputFilename))
+			{
+				await File.WriteAllTextAsync(inputFilename, outputText);
+			}
+			else if (!String.IsNullOrEmpty(inputFilename))
+			{
+				string nameWithoutExt = Path.GetFileNameWithoutExtension(inputFilename);
+				string timestamp = DateTime.Now.ToString("yyyy-MM-dd_HHmmss");
+				string extension = Path.GetExtension(inputFilename);
+				string newFileName = $"{nameWithoutExt}_{timestamp}{extension}";
+				await File.WriteAllTextAsync(newFileName, outputText);
+			}
+			else
+			{
+				await Console.Out.WriteAsync(outputText);
+			}
+
+			if (!quiet)
+				Console.WriteLine($"Done. {matchesCount} guids replaced, {pairsCount} pairs.");
+
+			return 0;
+		}
+
+		static string ReplaceGuids(string text, out int matchesCount, out int pairsCount)
+		{
+			string pattern = "[a-fA-F0-9]{8}-([a-fA-F0-9]{4}-){3}[a-fA-F0-9]{12}";
+
+			Dictionary<Guid, Guid> pairs = new Dictionary<Guid, Guid>();
+			MatchCollection matches = Regex.Matches(text, pattern);
+
+			matchesCount = matches.Count;
+			pairsCount = 0;
+
+			if (matchesCount <= 0)
+				return null;
+
+			int lastStart = 0;
+			StringBuilder sb = new StringBuilder(text.Length);
+
+			Match firstMatch = matches[0];
+			string firstString = text.Substring(firstMatch.Index, 36);
+			char firstLetter = firstString.SkipWhile(x => Char.IsDigit(x)).First();
+			bool isUpper = Char.IsUpper(firstLetter);
+
+			foreach (Match m in matches)
+			{
+				sb.Append(text.Substring(lastStart, m.Index - lastStart));
+				Guid oldGuid = Guid.Parse(m.Value);
+
+				if (!pairs.ContainsKey(oldGuid))
+					pairs.Add(oldGuid, Guid.NewGuid());
+
+				Guid newGuid = pairs[oldGuid];
+				string newString = newGuid.ToString();
+
+				if (isUpper)
+					newString = newString.ToUpper();
+
+				sb.Append(newString);
+				lastStart = m.Index + m.Length;
+			}
+
+			pairsCount = pairs.Count;
+			sb.Append(text.Substring(lastStart));
+			return sb.ToString();
+		}
+	}
+}
